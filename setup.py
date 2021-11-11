d2901afa302b5791dd9152e7936d042ccca4d8b3
TinQlo <orisui@icloud.com>
2021-11-11 22:06:31 +0800

    workflow test add manual trigger(debug)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "dist"},
-    packages=setuptools.find_packages(where="dist"),
+    package_dir={"": "srcc"},
+    packages=setuptools.find_packages(where="srcc"),
     python_requires=">=3.6",
 )
