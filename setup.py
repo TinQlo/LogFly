53698d789c12334bd5cdcf86766570e532f0550c
TinQlo <orisui@icloud.com>
2021-11-11 22:09:09 +0800

    workflow test add manual trigger(debug)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="logfly",
-    version="0.7",
+    version="0.8",
     author="Yuan Sui",
     author_email="orisui@icloud.com",
     description="Aa simple log tool by python  ",
